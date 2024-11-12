5c3942df00ced9957832fddbe6e1ff6dbfa1629f
Oleg Bragin <oleg.bragin@icloud.com>
2024-11-12 16:44:27 +0300

    Add save method and tests

--- Sources/USkateApi/Controllers/CityRouteController.swift
+++ Sources/USkateApi/Controllers/CityRouteController.swift
@@ -4,12 +4,53 @@ import Vapor
 struct CityRouteController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let api = routes.grouped("api")
-        api.group("cityroutes") { cityroutes in
-            cityroutes.get(use: index)
+        api.group("cityroutes") { cityRoutes in
+            cityRoutes.get(use: index)
+
+            cityRoutes.post(use: create)
+
+            cityRoutes.group(":id") { cityRoute in
+                cityRoute.patch(use: update)
+                cityRoute.delete(use: delete)
+            }
         }
     }
 
     func index(req: Request) async throws -> [CityRoute] {
         try await CityRoute.query(on: req.db).all()
+    }
+
+    func create(req: Request) async throws -> CityRoute {
+        let cityRouteDTO = try req.content.decode(CityRoute.self)
+        let cityRoute = CityRoute()
+        cityRoute.title = cityRouteDTO.title
+        cityRoute.details = cityRouteDTO.details
+        cityRoute.path = cityRouteDTO.path
+        
+        try await cityRoute.save(on: req.db)
+        return cityRoute
+    }
+
+    func update(req: Request) async throws -> CityRoute {
+        guard let cityRoute = try await CityRoute.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+
+        let cityRouteDTO = try req.content.decode(CityRoute.self)
+        cityRoute.title = cityRouteDTO.title
+        cityRoute.details = cityRouteDTO.details
+        cityRoute.path = cityRouteDTO.path
+        
+        try await cityRoute.update(on: req.db)
+        return cityRoute
+    }
+
+    func delete(req: Request) async throws -> HTTPStatus {
+        guard let cityRoute = try await CityRoute.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        
+        try await cityRoute.delete(on: req.db)
+        return .noContent
     }
 }
--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -110,4 +110,92 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.status, .ok)
         })
     }
+
+    func testCreateCityRoute() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.POST, "api/cityroutes", beforeRequest: { req in
+            let newCityRoute = CityRoute()
+            newCityRoute.title = "Mexican SPb"
+            newCityRoute.details = "Feel Mexico"
+            newCityRoute.path = "http://yandex.ru"
+            try req.content.encode(newCityRoute)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            let cityRoute = try res.content.decode(CityRoute.self)
+            XCTAssertEqual(cityRoute.title, "Mexican SPb")
+        })
+    }
+
+    func testDeletenewCityRoute() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        var cityRouteID: Int?
+
+        try app.test(.POST, "api/cityroutes", beforeRequest: { req in
+            let newCityRoute = CityRoute()
+            newCityRoute.title = "Mexican SPb"
+            newCityRoute.details = "Feel Mexico"
+            newCityRoute.path = "http://yandex.ru"
+            try req.content.encode(newCityRoute)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            let newCityRoute = try res.content.decode(CityRoute.self)
+            cityRouteID = newCityRoute.id
+        })
+
+        XCTAssertNotNil(cityRouteID)
+
+        guard let cityRouteID = cityRouteID else {
+            return
+        }
+
+        try app.test(.DELETE, "api/cityroutes/\(cityRouteID)", afterResponse: { res in
+            XCTAssertEqual(res.status, .noContent)
+        })
+    }
+
+    func testUpdateCityRoute() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        var cityRouteID: Int?
+
+        try app.test(.POST, "api/cityroutes", beforeRequest: { req in
+            let newCityRoute = CityRoute()
+            newCityRoute.title = "Mexican SPb"
+            newCityRoute.details = "Feel Mexico"
+            newCityRoute.path = "http://yandex.ru"
+            try req.content.encode(newCityRoute)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            let cityRoute = try res.content.decode(CityRoute.self)
+            cityRouteID = cityRoute.id
+        })
+
+        XCTAssertNotNil(cityRouteID)
+
+        guard let cityRouteID = cityRouteID else {
+            return
+        }
+
+        try app.test(.PATCH, "api/cityroutes/\(cityRouteID)", beforeRequest: { req in
+            let updatedCityRoute = CityRoute()
+            updatedCityRoute.id = cityRouteID
+            updatedCityRoute.title = "Spanish SPb"
+            updatedCityRoute.details = "Feel Spain"
+            updatedCityRoute.path = "http://vk.com"
+            try req.content.encode(updatedCityRoute)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            
+            let cityRoute = try res.content.decode(CityRoute.self)
+            XCTAssertEqual(cityRoute.title, "Spanish SPb")
+        })
+    }
 }
