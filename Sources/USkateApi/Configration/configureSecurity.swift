82e5d1a79bcc0c80272bfabe45585573a21d5826
Oleg Bragin <oleg.bragin@icloud.com>
2025-03-06 21:42:15 +0300

    Update project

--- Sources/USkateApi/Configration/configureSecurity.swift
+++ Sources/USkateApi/Configration/configureSecurity.swift
@@ -5,10 +5,10 @@ func configureSecurity(_ app: Application) throws {
     try app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
         certificateChain: [
             .certificate(.init(
-                file: "cert.pem",
+                file: "/etc/ssl/certs/ssl-cert-snakeoil.pem",
                 format: .pem
             ))
         ],
-        privateKey: .file("key.pem")
+        privateKey: .privateKey(.init(file: "/etc/ssl/private/ssl-cert-snakeoil.key", format: .pem))
     )
   }
